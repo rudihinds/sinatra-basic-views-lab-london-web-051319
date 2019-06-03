1007d074fea038e565a0009f8ec44fbca067735c
rudi hinds <rudihinds@gmail.com>
2019-06-03 17:43:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  
+  get "/" do
+    
 
 end
+
+In `app.rb`, you'll want to create a controller action responds to a GET request and loads `index.erb` in the browser.
+<p data-visibility='hidden'>View <a href='https://learn.co/lessons/sinatra-basic-views-lab' title='Sinatra Basic Views Lab'>Sinatra Basic Views Lab</a> on Learn.co and start learning to code for free.</p>
